9b0f95bce578b055d48f6cbc9b5bdefaf8bcbd54
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-15 10:20:10 +0100

    chore: Api is now obsolete

--- cogs/Api.py
+++ cogs/Api.py
@@ -1,9 +1,9 @@
-from os import getenv, putenv
+# from os import getenv, putenv
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
+# from dotenv import load_dotenv
 from assets.send_message import send_message
 
 # NOTE: Not secure and doesn't work, think to delete this command
@@ -15,35 +15,46 @@ class Api(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    load_dotenv()
-    debug_guilds = [int(getenv("DEBUG_GUILD"))]
+    #    load_dotenv()
+    #    debug_guilds = [int(getenv("DEBUG_GUILD"))]
 
     @commands.slash_command(
-        name="api",
-        description="Set your API key",
-        guild_ids=debug_guilds,
+        #        name="api",
+        #        description="Set your API key",
+        #        guild_ids=debug_guilds,
     )
-    async def api(self, interaction: discord.Interaction, key: discord.Option(str)):
+    async def api(
+        self, interaction: discord.Interaction
+    ):  # , key: discord.Option(str)):
         # Check if the key is valid
-        with open(".env", "r") as f:
-            for line in f.readlines():
-                try:
-                    key = key
-                    name = "API_KEY_" + str(interaction.guild_id)
-
-                    name, key = line.split("=")
-                    putenv(name, key)
-                except ValueError:
-                    # syntax error
-                    pass
-
         await send_message(
             interaction,
-            "API key set",
-            "Your API key is now set !",
-            "yellow",
+            "Obsolete command",
+            "This command is not secure and doesn't work. Please use the .env file instead.",
+            "error",
             is_ephemeral=True,
         )
+
+
+#        with open(".env", "r") as f:
+#            for line in f.readlines():
+#                try:
+#                    key = key
+#                    name = "API_KEY_" + str(interaction.guild_id)
+#
+#                    name, key = line.split("=")
+#                    putenv(name, key)
+#                except ValueError:
+#                    # syntax error
+#                    pass
+#
+#        await send_message(
+#            interaction,
+#            "API key set",
+#            "Your API key is now set !",
+#            "info",
+#            is_ephemeral=True,
+#        )
 
 
 def setup(client):
